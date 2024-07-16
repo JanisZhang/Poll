98bbe69204ecbbc781a020a9de097d649d430712
JiadiZhang <janiszhang.se@gmail.com>
2024-07-16 15:00:04 +0800

    Use generic views

--- polls/views.py
+++ polls/views.py
@@ -4,8 +4,9 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from .models import Question, Choice
+from django.views import generic
 
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -20,11 +21,11 @@ def index(request):
     # return render(request, 'polls/index.html',context)
 
 def details(request, question_id):
-    try:
-        # question = Question.objects.get(pk=question_id)
-        question = get_object_or_404(Question, pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request,"polls/detail.html",{"question":question})
     # return HttpResponse("You're looking at question %s." % question_id)
 
@@ -33,6 +34,29 @@ def results(request, question_id):
     return render(request,"polls/results.html",{"question":question})
     # response = "You're looking at question %s."
     # return HttpResponse(response % question_id)
+
+
+"""
+Add generic view
+"""
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
